a8019e1ffe422ce3f51af91cbd41e3abe7f54bef
Ryan Faulhaber <faulhaberryan@gmail.com>
2020-08-14 22:33:51 -0400

    Clean up

--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,6 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 use std::ffi::CString;
 use std::os::raw;
 
-type EmacsFn = unsafe extern "C" fn(
-    env: *mut emacs_env,
-    nargs: isize,
-    args: *mut emacs_value,
-    arg1: *mut ::std::os::raw::c_void,
-) -> emacs_value;
-
 #[no_mangle]
 #[allow(non_upper_case_globals)]
 pub static plugin_is_GPL_compatible: libc::c_int = 0;
@@ -32,7 +25,6 @@ pub extern "C" fn get_environment(ert: *mut emacs_runtime) -> *mut emacs_env {
 pub unsafe extern "C" fn emacs_module_init(ert: *mut emacs_runtime) -> libc::c_int {
     let env = get_environment(ert);
 
-    println!("making function");
     let make_function = (*env).make_function.expect("cannot get make_function");
     let my_message = make_function(
         env,
@@ -48,7 +40,7 @@ pub unsafe extern "C" fn emacs_module_init(ert: *mut emacs_runtime) -> libc::c_i
     let my_sum = make_function(
         env,
         0,
-        10,
+        emacs_variadic_function as isize,
         Some(my_sum),
         CString::new("This will add all the numbers you pass to it!")
             .unwrap()
@@ -58,12 +50,9 @@ pub unsafe extern "C" fn emacs_module_init(ert: *mut emacs_runtime) -> libc::c_i
 
     let intern = (*env).intern.expect("could not get intern in main");
 
-    println!("interning my-rust-func");
     let fn_name = intern(env, CString::new("my-message-from-rust").unwrap().as_ptr());
-    println!("interning my-rust-mod");
     let mod_name = intern(env, CString::new("my-rust-mod").unwrap().as_ptr());
 
-    println!("interning sum_name");
     let sum_name = intern(env, CString::new("my-sum").unwrap().as_ptr());
 
     let fset = intern(env, CString::new("fset").unwrap().as_ptr());
@@ -75,25 +64,13 @@ pub unsafe extern "C" fn emacs_module_init(ert: *mut emacs_runtime) -> libc::c_i
     let provide_args = [mod_name].as_mut_ptr();
 
     let funcall = (*env).funcall.expect("cannot get funcall");
-    println!("calling fset");
     funcall(env, fset, 2, fset_args);
-    println!("calling fset again");
     funcall(env, fset, 2, fset_second_args);
-    println!("calling provide");
     funcall(env, provide, 1, provide_args);
-
-    println!("end");
 
     0
 }
 
-unsafe fn provide(env: *mut emacs_env, feature: &str) {
-    let feat = intern_sym(env, feature);
-    let args = [feat].as_mut_ptr();
-    funcall(env, "provide", 1, args);
-}
-
-#[no_mangle]
 unsafe extern "C" fn message_from_rust(
     env: *mut emacs_env,
     nargs: isize,
@@ -118,13 +95,13 @@ unsafe extern "C" fn my_sum(
         .extract_integer
         .expect("could not get extract_integer");
 
-    let mut ints = Vec::new();
+    let mut numbers = Vec::new();
 
     for i in 0..nargs {
-        ints.push(extract_integer(env, *args.offset(i)));
+        numbers.push(extract_integer(env, *args.offset(i)));
     }
 
-    let sum = ints.iter().fold(0, |acc, n| acc + n);
+    let sum = numbers.iter().fold(0, |acc, n| acc + n);
 
     let make_integer = (*env).make_integer.expect("could not get make_integer");
 
@@ -159,8 +136,4 @@ pub unsafe extern "C" fn funcall(
     let qf = intern_sym(env, name);
     let funcall = (*env).funcall.expect("cannot get funcall");
     funcall(env, qf, nargs, args)
-}
-
-fn make_str(s: &str) -> *const i8 {
-    CString::new(s).unwrap().as_ptr()
 }
